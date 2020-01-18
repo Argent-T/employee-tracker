82743dbdd1eeb5db7a44d17e6b5e1d654f3d1c0d
JesterPanda <55069246+Jesterpanda@users.noreply.github.com>
2020-01-17 22:02:05 -0500

    first options working

--- database.sql
+++ database.sql
@@ -20,4 +20,37 @@ first_name varchar(30) not null,
 last_name varchar(30) not null,
 role_id int not null,
 manager_id int
-)
+);
+
+insert into department (name) values
+("Sales"),
+("Engineering"),
+("Finance"),
+("Legal")
+;
+select * from department;
+
+insert into role (title, salary, department_id) values
+("Sales Lead", 100000, 1),
+("Salesperson", 80000, 1),
+("Lead Engineer", 150000,2),
+("Software Engineer", 120000,2),
+("Accountant", 125000,3),
+("Legal Team Lead", 250000, 4),
+("Lawyer", 190000,4)
+;
+select * from role;
+
+insert into employee (first_name, last_name, role_id) values
+("Ashley", "Rodriguez", 3),
+("Malia", "Brown",5),
+("Sarah", "Lourd",6);
+
+insert into employee (first_name, last_name, role_id, manager_id) values
+("John", "Doe", 1, 1),
+("Mike", "Chan",2,4),
+("Kevin","Tupik",4,1),
+("Tom","Allen",7,3);
+
+
+select * from employee
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const cTable = require('console.table');
+var async = require("async");
+
 var connection = mysql.createConnection({
   host: "localhost",
 
@@ -14,28 +17,138 @@ var connection = mysql.createConnection({
   database: "contentmanagementsystem_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
+  if (err) throw err;
+  // console.log("connected as id " + connection.threadId + "\n");
+});
+
+
+start()
+function start() {
+
+  console.log("-------------------------------------------------------------")
+  console.log("|           ***********          *     **                   |")
+  console.log("|           *                   * *   *  *                  |")
+  console.log("|           *                  *   * *    *                 |")
+  console.log("|           ***********        *    *     *                 |")
+  console.log("|           *                  *          *                 |")
+  console.log("|           *                  *          *                 |")
+  console.log("|           ***********        *          *                 |")
+  console.log("|            Employee            Management                 |")
+  console.log("-------------------------------------------------------------")
+
+
+
+
+  inquirer
+    .prompt({
+      name: "firstchoice",
+      type: "list",
+      message: "Would you like to do?",
+      choices: ["View all employees", "View all employees by department", "View all employees by manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "Exit"]
+    })
+    .then(function (answer) {
+
+      if (answer.firstchoice === "View all employees") {
+        viewEmployees();
+      }
+      else if (answer.firstchoice === "View all employees by department") {
+        viewByDepartment();
+      }
+      else if (answer.firstchoice === "View all employees by manager") {
+        viewByManager();
+      }
+      else if (answer.firstchoice === "Add Employee") {
+        addEmployee();
+      }
+      else if (answer.firstchoice === "Remove Employee") {
+        removeEmployee();
+      }
+      else if (answer.firstchoice === "Update Employee Role") {
+        updateRole();
+      }
+      else if (answer.firstchoice === "Update Employee Manager") {
+        updateManager();
+      }
+
+
+      else {
+        connection.end();
+      }
+    });
+}
+
+function viewEmployees() {
+  var query = "select employee.id, employee.first_name, employee.last_name, r.title, d.name as department, ";
+  query += "r.salary, concat(emp.first_name,' ', emp.last_name) as manager from employee ";
+  query += "inner join role r on r.id = employee.role_id inner join department d on d.id = r.department_id left join employee emp on employee.manager_id = emp.id"
+
+
+  connection.query(query, function (err, res) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
+    console.table(res);
+    connection.end();
   });
-  
-  function start() {
+
+}
+
+
+function viewByDepartment() {
+  var queryOptions = "select name from department";
+  var options = [];
+  connection.query(queryOptions, function (err, res) {
+    if (err) throw err;
+
+    for (var i = 0; i < res.length; i++) {
+      options.push(res[i].name)
+    }
     inquirer
       .prompt({
-        name: "firstchoice",
+        name: "departmentchoice",
         type: "list",
-        message: "Would you like to [ADD departments, roles, employees], [VIEW departments, roles, employees], or [EXIT]",
-        choices: ["ADD", "VIEW", "EXIT"]
-      })
-      .then(function(answer) {
-        // based on their answer, either call the bid or the post functions
-        if (answer.postOrBid === "ADD") {
-          addItem();
-        }
-        else if(answer.postOrBid === "VIEW") {
-          viewItem();
-        } else{
+        message: "Which department would you like to view?",
+        choices: options
+      }).then(function (answer) {
+        var query = "select employee.id, employee.first_name, employee.last_name, r.title, d.name as department, ";
+        query += "r.salary, concat(emp.first_name,' ', emp.last_name) as manager from employee ";
+        query += "inner join role r on r.id = employee.role_id inner join department d on d.id = r.department_id left join employee emp on employee.manager_id = emp.id where d.name = ?"
+        connection.query(query, [answer.departmentchoice], function (err, res) {
+          if (err) throw err;
+          console.table(res);
           connection.end();
-        }
-      });
-  }
+        })
+      })
+  })
+};
+
+function viewByManager() {
+  var options = []
+  var queryOptions = "select distinct concat(emp.first_name,' ', emp.last_name) as manager from employee inner join role r on r.id = employee.role_id inner join department d on d.id = r.department_id left join employee emp on employee.manager_id = emp.id";
+  connection.query(queryOptions, function (err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      if (res[i].manager != null){
+      options.push(res[i].manager)
+      }
+    }
+    inquirer
+      .prompt({
+        name: "manager choice",
+        type: "list",
+        message: "Which manager would you like to view?",
+        choices: options
+      }).then(function (answer) {
+        var query = "select employee.id, employee.first_name, employee.last_name, r.title, d.name as department, ";
+        query += "r.salary, concat(emp.first_name,' ', emp.last_name) as manager from employee ";
+        query += "inner join role r on r.id = employee.role_id inner join department d on d.id = r.department_id left join employee emp on employee.manager_id = emp.id where  ?"
+        connection.query(query, [answer.departmentchoice], function (err, res) {
+          if (err) throw err;
+          console.table(res);
+          connection.end();
+        })
+      })
+  });
+}
+
+
+
