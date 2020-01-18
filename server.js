d68af27e05636cf72ff75ab0f25237749052284e
JesterPanda <55069246+Jesterpanda@users.noreply.github.com>
2020-01-18 00:13:18 -0500

    almost done

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 const cTable = require('console.table');
-var async = require("async");
+const chalk = require('chalk');
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -23,22 +23,27 @@ connection.connect(function (err) {
 });
 
 
-start()
+init();
+
+function init() {
+
+  console.log(chalk.red("-------------------------------------------------------------"))
+  console.log(chalk.red("|           ***********          *     **                   |"))
+  console.log(chalk.yellow("|           *                   * *   *  *                  |"))
+  console.log(chalk.yellow("|           *                  *   * *    *                 |"))
+  console.log(chalk.green("|           ***********        *    *     *                 |"))
+  console.log(chalk.green("|           *                  *          *                 |"))
+  console.log(chalk.blue("|           *                  *          *                 |"))
+  console.log(chalk.blue("|           ***********        *          *                 |"))
+  console.log(chalk.magenta("|            Employee            Management                 |"))
+  console.log(chalk.magenta("-------------------------------------------------------------"))
+  start();
+}
+
+
+
+
 function start() {
-
-  console.log("-------------------------------------------------------------")
-  console.log("|           ***********          *     **                   |")
-  console.log("|           *                   * *   *  *                  |")
-  console.log("|           *                  *   * *    *                 |")
-  console.log("|           ***********        *    *     *                 |")
-  console.log("|           *                  *          *                 |")
-  console.log("|           *                  *          *                 |")
-  console.log("|           ***********        *          *                 |")
-  console.log("|            Employee            Management                 |")
-  console.log("-------------------------------------------------------------")
-
-
-
 
   inquirer
     .prompt({
@@ -78,6 +83,7 @@ function start() {
     });
 }
 
+// view all employees
 function viewEmployees() {
   var query = "select employee.id, employee.first_name, employee.last_name, r.title, d.name as department, ";
   query += "r.salary, concat(emp.first_name,' ', emp.last_name) as manager from employee ";
@@ -87,12 +93,12 @@ function viewEmployees() {
   connection.query(query, function (err, res) {
     if (err) throw err;
     console.table(res);
-    connection.end();
+    start();
   });
 
 }
 
-
+// view by department
 function viewByDepartment() {
   var queryOptions = "select name from department";
   var options = [];
@@ -115,40 +121,242 @@ function viewByDepartment() {
         connection.query(query, [answer.departmentchoice], function (err, res) {
           if (err) throw err;
           console.table(res);
-          connection.end();
+          start();
         })
       })
   })
 };
 
+// view by manager
 function viewByManager() {
   var options = []
   var queryOptions = "select distinct concat(emp.first_name,' ', emp.last_name) as manager from employee inner join role r on r.id = employee.role_id inner join department d on d.id = r.department_id left join employee emp on employee.manager_id = emp.id";
   connection.query(queryOptions, function (err, res) {
     if (err) throw err;
     for (var i = 0; i < res.length; i++) {
-      if (res[i].manager != null){
-      options.push(res[i].manager)
+      if (res[i].manager != null) {
+        options.push(res[i].manager)
       }
     }
     inquirer
       .prompt({
-        name: "manager choice",
+        name: "managerchoice",
         type: "list",
         message: "Which manager would you like to view?",
         choices: options
+
+        // get manager id from name
       }).then(function (answer) {
-        var query = "select employee.id, employee.first_name, employee.last_name, r.title, d.name as department, ";
-        query += "r.salary, concat(emp.first_name,' ', emp.last_name) as manager from employee ";
-        query += "inner join role r on r.id = employee.role_id inner join department d on d.id = r.department_id left join employee emp on employee.manager_id = emp.id where  ?"
-        connection.query(query, [answer.departmentchoice], function (err, res) {
+        let names = answer.managerchoice.split(" ")
+        var query = "select employee.id from employee where first_name = ? and last_name = ?";
+
+        connection.query(query, [names[0], names[1]], function (err, res1) {
           if (err) throw err;
-          console.table(res);
-          connection.end();
+          manager(res1)
+          // connection.end();
         })
       })
   });
 }
+// since i cant figure out async 
+function manager(res1) {
+  // console.log(res1)
+  var query = "select employee.id, employee.first_name, employee.last_name, r.title, d.name as department, ";
+  query += "r.salary, concat(emp.first_name,' ', emp.last_name) as manager from employee ";
+  query += "inner join role r on r.id = employee.role_id inner join department d on d.id = r.department_id left join employee emp on employee.manager_id = emp.id where employee.manager_id = ?"
+  connection.query(query, res1[0].id, function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    start();
+  });
+}
+
+
+// add employee
+function addEmployee() {
+  var options = []
+  var queryOptions = "select title from role";
+  connection.query(queryOptions, function (err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      options.push(res[i].title)
+    }
+
+    inquirer.prompt([
+      {
+        name: "newfirstname",
+        type: "input",
+        message: "enter first name"
+      },
+      {
+        name: "newlastname",
+        type: "input",
+        message: "enter last name"
+      },
+      {
+        name: "rolechoice",
+        type: "list",
+        message: "Which role are they in?",
+        choices: options
+      },
+      {
+        name: "hasmanager",
+        type: "list",
+        message: "do they have a manager?",
+        choices: ["Yes", "No"]
+      }
+    ]).then(function (answer) {
+      let result = answer;
+      console.log(result)
+
+      // If no manager
+      if (answer.hasmanager === "No") {
+        var queryrole = "select id from role where title = ?";
+        var queryadd = "insert into employee (first_name, last_name, role_id) values (?,?,?)";
+        connection.query(queryrole, answer.rolechoice, function (err, res) {
+          if (err) throw err;
+          // console.log(res[0].id)
+          connection.query(queryadd, [answer.newfirstname, answer.newlastname, res[0].id], function (err, res) {
+            if (err) throw err;
+            console.log(`Added ${answer.newfirstname} ${answer.newlastname} as a ${answer.rolechoice}.`);
+            start();
+          })
+        })
+      }
+      // If has a manager list names 
+      else {
+        var querynames = "select id, concat(first_name,' ', last_name) as name from employee";
+        let names = [];
+        connection.query(querynames, function (err, res) {
+          if (err) throw err;
+          for (var i = 0; i < res.length; i++) {
+            names.push(res[i].name)
+          }
+
+          inquirer.prompt({
+            name: "manager",
+            type: "list",
+            message: "Select a Manager",
+            choices: names
+
+            // Turn role and manager into IDs and then run insert
+          }).then(function (answer) {
+            let names = answer.manager.split(" ")
+            var queryroleId = "select id from role where title = ?";
+            var queryManId = "select employee.id from employee where first_name = ? and last_name = ?";
+            var queryadd = "insert into employee (first_name, last_name, role_id, manager_id) values (?,?,?,?)"
+
+            connection.query(queryManId, [names[0], names[1]], function (err, managerId) {
+              if (err) throw err;
+              managerId = managerId[0].id;
+              connection.query(queryroleId, result.rolechoice, function (err, res) {
+                if (err) throw err;
+                roleId = res[0].id;
+                // console.log("role ID")
+                // console.log(roleId)
+                connection.query(queryadd, [result.newfirstname, result.newlastname, roleId, managerId], function (err, res) {
+                  if (err) throw err;
+                  console.log(`Added ${result.newfirstname} ${result.newlastname} as a ${result.rolechoice} with ${answer.manager} as their manager.`);
+                  start();
+                })
+              })
+            })
+          })
+        })
+      }
+    })
+  })
+}
+
+
+// Remove employee
+function removeEmployee() {
+  var querynames = "select id, concat(first_name,' ', last_name) as name from employee";
+  let names = [];
+  connection.query(querynames, function (err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      names.push(res[i].name)
+    }
+
+    inquirer.prompt([{
+      name: "remove",
+      type: "list",
+      message: "Who would you like to remove?",
+      choices: names
+    },
+    {
+      name: "confirm",
+      type: "list",
+      message: "Are you sure?",
+      choices: ["Yes", "No"]
+    }
+
+    ]).then(function (answer) {
+      if (answer.confirm === "Yes") {
+        let names = answer.remove.split(" ")
+        var queryNameId = "select employee.id from employee where first_name = ? and last_name = ?";
+        var queryRemove = "delete from employee where id = ?"
+
+        connection.query(queryNameId, [names[0], names[1]], function (err, empId) {
+          if (err) throw err;
+          connection.query(queryRemove, empId[0].id, function (err, res) {
+            if (err) throw err;
+            console.log(`Removed ${answer.remove} from the database`);
+            start();
+
+          })
+        })
+      }
+      else {
+        start();
+
+      }
+
+    })
+  })
+}
 
 
 
+function updateRole() {
+  var querynames = "select id, concat(first_name,' ', last_name) as name from employee";
+  var queryRoles = "select id, title from role";
+  let names = [];
+  let roles = [];
+  connection.query(querynames, function (err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      names.push(res[i])
+    }
+    console.log(names)
+    connection.query(queryRoles, function (err, res) {
+      if (err) throw err;
+      for (var i = 0; i < res.length; i++) {
+        roles.push(res[i])
+      }
+      console.log(roles)
+    })
+
+    inquirer.prompt([
+      {
+        name: "namechoice",
+        type: "list",
+        message: "Whos role would you like to update?",
+        choices: names
+      },
+      {
+        name: "confirm",
+        type: "list",
+        message: "Are you sure?",
+        choices: roles
+      }
+
+    ])
+
+
+
+
+
+  })
+}
